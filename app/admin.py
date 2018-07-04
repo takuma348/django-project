7d1f824e069f5bbd25a286faa3900c0e71200104
python takuma <go.ahead.mabuhay@gmail.com>
2018-07-04 09:33:14 +0900

    first all commit

---
+++ app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+"""
+    class Meta:
+        verbose_name = 'ユーザ'
+        verbose_name_plural = 'ユーザ'
+"""
